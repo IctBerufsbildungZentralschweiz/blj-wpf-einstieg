078fbede75d92edfb103b873663098f6a7d4257b
VFI\nuu <urs.nussbaumer@ict-bz.ch>
2018-04-19 10:04:16 +0200

    completed ComboBox-und-ListBox_MVVM

---
+++ Musterloesungen/ComboBox-und-ListBox_MVVM/ViewModels/MainViewModel.cs
@@ -0,0 +1,66 @@
+﻿using ComboBox_und_ListBox_MVVM.Common;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ComboBox_und_ListBox_MVVM.ViewModels
+{
+    class MainViewModel : ViewModelBase
+    {
+        public DelegateCommand<object> AddCommand { get; set; }
+        public DelegateCommand<object> RemoveCommand { get; set; }
+
+        public ObservableCollection<string> FoodList { get; set; }
+        public ObservableCollection<string> BreakfastList { get; set; }
+
+        public string SelectedFood { get; set; }
+        public string SelectedBreakfast { get; set; }
+
+        public MainViewModel()
+        {
+            AddCommand = new DelegateCommand<object>(ExecuteAdd, CanExecuteAdd);
+            RemoveCommand = new DelegateCommand<object>(ExecuteRemove, CanExecuteRemove);
+
+            FoodList = new ObservableCollection<string>();
+            BreakfastList = new ObservableCollection<string>();
+
+            LoadData();
+        }
+
+        private void LoadData()
+        {
+            FoodList.Add("Eine Scheibe Brot");
+            FoodList.Add("Apfel");
+            FoodList.Add("Rührei mit Speck");
+            FoodList.Add("Ein Gipfeli");
+            FoodList.Add("Eine Tasse Tee");
+            FoodList.Add("Ein Birchermüsli");
+            FoodList.Add("Eine Tasse Kaffee");
+        }
+
+        public void ExecuteAdd(object o)
+        {
+            BreakfastList.Add(SelectedFood);
+            FoodList.Remove(SelectedFood);
+        }
+
+        public bool CanExecuteAdd(object o)
+        {
+            return !string.IsNullOrEmpty(SelectedFood);
+        }
+
+        public void ExecuteRemove(object o)
+        {
+            FoodList.Add(SelectedBreakfast);
+            BreakfastList.Remove(SelectedBreakfast);
+        }
+
+        public bool CanExecuteRemove(object o)
+        {
+           return !string.IsNullOrEmpty(SelectedBreakfast);
+        }
+    }
+}
---
+++ Musterloesungen/ComboBox-und-ListBox_MVVM/Views/MainWindow.xaml.cs
@@ -0,0 +1,48 @@
+﻿using ComboBox_und_ListBox_MVVM.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace ComboBox_und_ListBox_MVVM.Views
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            this.DataContext = new MainViewModel();
+        }
+
+        private void ListBox_GotFocus(object sender, RoutedEventArgs e)
+        {
+            /* 
+             * ListBox und ComboBox implementieren ICommandSource nicht; d.h. für Elemente dieses Typs
+             * ist Command-Binding nicht möglich. MVVM-Puristen, die Code-Behind konsequent vermeiden möchten
+             * müssen sich mit Implementation einer Depedency Property behelfen; ein Beispiel hier: 
+             * https://www.codeproject.com/Articles/363568/Command-binding-with-Events-a-way-from-simple-to-a 
+             * 
+             * Für mich (Urs Nussbaumer) ist es total OK, wenn wir hier Code-Behind schreiben; 
+             * denn es ist ja sehr View-spezifisches Verhalten, was wir hier implementieren.  
+            */
+            RemoveButton.IsDefault = true;
+        }
+
+        private void ComboBox_GotFocus(object sender, RoutedEventArgs e)
+        {
+            AddButton.IsDefault = true;
+        }
+    }
+}

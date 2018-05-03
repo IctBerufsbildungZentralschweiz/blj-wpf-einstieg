705b308c145336a373c7202f7a9c3b3aba531b7e
Urs Nussbaumer <urs.nussbaumer@ict-bz.ch>
2018-05-03 11:30:31 +0200

    Kaffeetasse added

---
+++ Musterloesungen/Kaffeetasse/ImageModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kaffeetasse
+{
+    public class ImageModel
+    {   
+        public string ImagePath { get; set; }
+        public string Type { get; set; }
+
+        public ImageModel(string imagePath, string type)
+        {
+            ImagePath = imagePath;
+            Type = type;
+        }
+    }
+}

4ff386ff2d444be7fbd53650b4bf356072b98c9b
Urs Nussbaumer <urs.nussbaumer@ict-bz.ch>
2018-05-14 14:11:59 +0200

    completed UpperCaseConverter.

---
+++ Musterloesungen/04 UpperCaseConverter/Person.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _04_UpperCaseConverter
+{
+    class Person : INotifyPropertyChanged
+    {
+        string _firstName;
+        public string FirstName
+        {
+            get { return _firstName; }
+            set { _firstName = value; OnPropertyChanged("FirstName"); OnPropertyChanged("FullName"); }
+        }
+
+        string _lastName;
+        public string LastName
+        {
+            get { return _lastName; }
+            set { _lastName = value; OnPropertyChanged("LastName"); OnPropertyChanged("FullName"); }
+        }
+
+        public string FullName
+        {
+            get { return _firstName + " " + _lastName; }
+        }
+
+        
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected virtual void OnPropertyChanged(string propertyName)
+        {
+            if (PropertyChanged != null)
+                PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
---
+++ Musterloesungen/04 UpperCaseConverter/UpperCaseConverter.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+
+namespace _04_UpperCaseConverter
+{
+    class UpperCaseConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value != null)
+                return value.ToString().ToUpper();
+
+            return string.Empty;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            // die Rückumwandlung wäre relativ schwierig zu bewerkstelligen, lassen wir sie darum weg, da sie eh nicht benötigt wird
+            return value;
+        }
+    }
+}
